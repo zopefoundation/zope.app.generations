5bf112de6717ed5406e5288ee388875e360ea353
Jim Fulton <jim@zope.com>
2004-04-22 18:58:10 +0000

    Implemented a framework for orderly evolution of ZODB application/database schemas:
    http://dev.zope.org/Zope3/DatabaseGenerations
    
    Included a schema manager for zope itself that simply establishes a
    generation of 0.

---
+++ demo/__init__.py
@@ -0,0 +1,30 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Demo package for evolution scripts
+
+The evolution scripts in this package are pretty dumb. The just call
+the evolve function defined here with a generation number. 
+
+$Id: __init__.py,v 1.1 2004/04/22 18:58:10 jim Exp $
+"""
+
+key = 'zope.app.generations.demo-generation'
+
+def evolve(context, generation):
+    """Demo that "evolves" a database.
+
+    All it does is write the generation to a database root item.
+    """
+    root = context.connection.root()
+    root[key] = root.get(key, ()) + (generation, )
