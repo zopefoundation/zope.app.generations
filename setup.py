9a2ff2cd0e96d99e5868f7fdb219c1851fd64054
Christian Theune <ct@gocept.com>
2007-04-22 10:35:04 +0000

    Updating version.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import os
 from setuptools import setup, find_packages, Extension
 
 setup(name='zope.app.generations',
-      version='3.4dev',
+      version = '3.4.0a1',
       url='http://svn.zope.org/zope.app.generations',
       license='ZPL 2.1',
       description='Zope generations',
