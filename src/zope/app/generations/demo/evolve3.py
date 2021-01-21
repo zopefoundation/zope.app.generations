3692fba5cc42c9c969590e28b1095ba00adf4628
Michael Howitz <mh@gocept.com>
2021-01-21 15:43:33 +0100

    Configuring for pure-python

---
+++ src/zope/app/generations/demo/evolve3.py
@@ -0,0 +1,22 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Silly demo evolution module."""
+import zope.app.generations.demo
+
+
+generation = 3
+
+
+def evolve(context):
+    zope.app.generations.demo.evolve(context, generation)
